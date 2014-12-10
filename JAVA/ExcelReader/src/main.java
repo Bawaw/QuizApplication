aaf83046ce708a93e6deaaa92ca2ecfecb775b2b
Balder <gaming.anon@yandex.com>
2014-12-10 20:07:58 +0100

    jars
    excel reader

--- JAVA/ExcelReader/src/main.java
+++ JAVA/ExcelReader/src/main.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileInputStream;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -17,8 +19,16 @@ public class main {
 					"vragen-uit-excel.xlsx"));
 			Workbook wb = new XSSFWorkbook(file);
 			Sheet sheet = wb.getSheetAt(0);
-			int rowsCount = sheet.getLastRowNum();
-            System.out.println("Total Number of Rows: " + (rowsCount + 1));
+	
+			 for (int i = 0; i <= sheet.getLastRowNum(); i++) {
+	                Row row = sheet.getRow(i);
+	                int colCounts = row.getLastCellNum();
+	                System.out.println("Total Number of Cols: " + colCounts);
+	                for (int j = 0; j < colCounts; j++) {
+	                    Cell cell = row.getCell(j);
+	                    System.out.println("[" + i + "," + j + "]=" + cell.getStringCellValue());
+	                }
+	            }
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
